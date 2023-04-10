65b7fc2fd370280186a066cfe8d9885e89aaa392
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2023-04-09 22:06:41 -0700

    initial commit

---
+++ ShopOnline/ShopOnlineSolution/ShopOnline.Models/Dtos/CartItemDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnline.Models.Dtos
+{
+    public class CartItemDto
+    {
+        public int Id { get; set; } 
+        public int ProductId { get; set; }
+        public int CartId { get; set; }
+        public string? ProdcuctName { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceTotal { get; set;}
+        public int Qty { get; set; }
+    }
+}
